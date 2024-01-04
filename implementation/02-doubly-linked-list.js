edae25e41062f7196d7a4debb2805b37d4c49e89
Dana <dana.shaw95@gmail.com>
2024-01-03 18:54:18 -0600

    02 Complete

--- implementation/02-doubly-linked-list.js
+++ implementation/02-doubly-linked-list.js
@@ -19,9 +19,9 @@ class DoublyLinkedList {
 
         // Add node of val to head of linked list
         let newNode = new DoublyLinkedNode(val);
-
-        if (this.length >= 0) {
-            this.head.previous = newNode;
+        
+        if (this.length > 0) {
+            this.head.prev = newNode;
             newNode.next = this.head;
             this.head = newNode;
         } else {
@@ -32,46 +32,91 @@ class DoublyLinkedList {
         this.length++;
 
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
     addToTail(val) {
         // Add node of val to tail of linked list
-
-        // Your code here 
+        const newNode = new DoublyLinkedNode(val)
+        
+        if(!this.tail){
+            this.head = newNode
+            this.tail = newNode
+            this.length++
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+            this.length++
+        }
 
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
     removeFromHead() {
         // Remove node at head
+        if(!this.head) return
+        
+        let oldHead = this.head
 
-        // Your code here 
-
+        if(this.head.next){
+            let newHead = this.head.next
+            
+            newHead.prev = null
+            this.head = newHead
+        } else {
+            this.head = null
+        }
+        this.length--
+        return oldHead.value
+        
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
     removeFromTail() {
         // Remove node at tail
+        if(!this.tail) return
 
-        // Your code here 
+        if(this.tail){
+            let oldTail = this.tail
+            
+            if(!this.tail.prev){
+                this.head = null
+                this.tail = null
+                this.length--
+                return oldTail.value
+            }
 
+            let newTail = this.tail.prev
+            
+            newTail.next = null
+            this.tail = newTail
+            this.length--
+            return oldTail.value
+        }
+        
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
     peekAtHead() {
         // Return value of head node
 
-        // Your code here 
+        return this.head ? this.head.value : undefined
 
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
     peekAtTail() {
         // Return value of tail node
 
-        // Your code here 
+        return this.tail ? this.tail.value : undefined
 
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 }
 
--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -11,17 +11,41 @@ class Queue {
     enqueue(val) {
         // Add node to end of queue (linked list)
 
-        // Your code here 
+        const newNode = new SinglyLinkedNode(val);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++
+        return this.length;
 
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
 
-        // Your code here 
+        if(!this.tail) return null
 
+        let oldHead = this.head
+
+        if(this.head.next){
+            this.head = this.head.next
+        } else {
+            this.head = null
+            this.tail = null
+        }
+        
+        this.length--
+        return oldHead.value
         // Write your hypothesis on the time complexity of this method here
+        //O(1)
     }
 
 }
